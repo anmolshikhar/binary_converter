e16853ab5af1d68c2991fc87430327af9758f2e3
Anmol Shikhar <anmolshikhar2323@gmail.com>
2025-04-15 14:23:22 +0530

    first of code

---
+++ converter.js
@@ -0,0 +1,15 @@
+let input = 0;
+let i=0;
+function inputfun(){
+    input = document.getElementById("inputValue").value;
+   let out = input;
+   document.getElementById("outPut").innerText= out;
+while(input>0){
+    /*remainder = input%2;
+    input = input/2;*/
+   let remainder = Math.floor(remainder);
+    arr=[];
+    arr.push(remainder);
+    console.log(arr);
+    i++;
+}}
